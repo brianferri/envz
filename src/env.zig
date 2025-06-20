e2784f2a049610d41f053455f45a9ec3c55f0ada
Brian Ferri <me@brianferri.dev>
2025-06-20 10:02:06 +0200

    feat: add tests

--- src/env.zig
+++ src/env.zig
@@ -26,22 +26,107 @@ pub fn print(self: Env) void {
     }
 }
 
-test "load comptime" {
-    const file: []const u8 = @embedFile("env/.env");
-    @setEvalBranchQuota(100000);
+const testing = std.testing;
 
-    const env: Env = .loadComptime(file);
+test "env: basic parsing" {
+    @setEvalBranchQuota(10000);
+    const env = Env.loadFromPathComptime("env/.env");
+
+    std.debug.print("\n", .{});
     env.print();
+
+    try testing.expectEqualStrings("basic", env.get("BASIC").?);
+    try testing.expectEqualStrings("after_line", env.get("AFTER_LINE").?);
+
+    try testing.expectEqualStrings("", env.get("EMPTY").?);
+    try testing.expectEqualStrings("", env.get("EMPTY_SINGLE_QUOTES").?);
+    try testing.expectEqualStrings("", env.get("EMPTY_DOUBLE_QUOTES").?);
+    try testing.expectEqualStrings("", env.get("EMPTY_BACKTICKS").?);
+
+    try testing.expectEqualStrings("single_quotes", env.get("SINGLE_QUOTES").?);
+    try testing.expectEqualStrings("    single quotes    ", env.get("SINGLE_QUOTES_SPACED").?);
+    try testing.expectEqualStrings("double_quotes", env.get("DOUBLE_QUOTES").?);
+    try testing.expectEqualStrings("    double quotes    ", env.get("DOUBLE_QUOTES_SPACED").?);
+
+    try testing.expectEqualStrings("double \"quotes\" work inside single quotes", env.get("DOUBLE_QUOTES_INSIDE_SINGLE").?);
+    try testing.expectEqualStrings("{ port: $MONGOLAB_PORT}", env.get("DOUBLE_QUOTES_WITH_NO_SPACE_BRACKET").?);
+    try testing.expectEqualStrings("single 'quotes' work inside double quotes", env.get("SINGLE_QUOTES_INSIDE_DOUBLE").?);
+
+    try testing.expectEqualStrings("`backticks` work inside single quotes", env.get("BACKTICKS_INSIDE_SINGLE").?);
+    try testing.expectEqualStrings("`backticks` work inside double quotes", env.get("BACKTICKS_INSIDE_DOUBLE").?);
+    try testing.expectEqualStrings("backticks", env.get("BACKTICKS").?);
+    try testing.expectEqualStrings("    backticks    ", env.get("BACKTICKS_SPACED").?);
+    try testing.expectEqualStrings("double \"quotes\" work inside backticks", env.get("DOUBLE_QUOTES_INSIDE_BACKTICKS").?);
+    try testing.expectEqualStrings("single 'quotes' work inside backticks", env.get("SINGLE_QUOTES_INSIDE_BACKTICKS").?);
+    try testing.expectEqualStrings("double \"quotes\" and single 'quotes' work inside backticks", env.get("DOUBLE_AND_SINGLE_QUOTES_INSIDE_BACKTICKS").?);
+
+    try testing.expectEqualStrings("expand\nnew\nlines", env.get("EXPAND_NEWLINES").?);
+    try testing.expectEqualStrings("dontexpand\\nnew\\nline", env.get("DONT_EXPAND_DOUBLE_ESCAPE").?);
+    try testing.expectEqualStrings("dontexpand\\nnewlines", env.get("DONT_EXPAND_UNQUOTED").?);
+    try testing.expectEqualStrings("dontexpand\\nnewlines", env.get("DONT_EXPAND_SQUOTED").?);
+
+    try testing.expectEqualStrings("inline comments", env.get("INLINE_COMMENTS").?);
+    try testing.expectEqualStrings("inline comments outside of #singlequotes", env.get("INLINE_COMMENTS_SINGLE_QUOTES").?);
+    try testing.expectEqualStrings("inline comments outside of #doublequotes", env.get("INLINE_COMMENTS_DOUBLE_QUOTES").?);
+    try testing.expectEqualStrings("inline comments outside of #backticks", env.get("INLINE_COMMENTS_BACKTICKS").?);
+    try testing.expectEqualStrings("inline comments start with a", env.get("INLINE_COMMENTS_SPACE").?);
+
+    try testing.expectEqualStrings("equals==", env.get("EQUAL_SIGNS").?);
+    try testing.expectEqualStrings("{\"foo\": \"bar\"}", env.get("RETAIN_INNER_QUOTES").?);
+    try testing.expectEqualStrings("{\"foo\": \"bar\"}", env.get("RETAIN_INNER_QUOTES_AS_STRING").?);
+    try testing.expectEqualStrings("{\"foo\": \"bar's\"}", env.get("RETAIN_INNER_QUOTES_AS_BACKTICKS").?);
+    try testing.expectEqualStrings("some spaced out string", env.get("TRIM_SPACE_FROM_UNQUOTED").?);
+    try testing.expectEqualStrings("therealnerdybeast@example.tld", env.get("USERNAME").?);
+    try testing.expectEqualStrings("parsed", env.get("SPACED_KEY").?);
 }
 
-test "load from path comptime" {
-    const env: Env = .loadFromPathComptime("env/.env");
-    env.print();
-}
+test "env: multiline parsing" {
+    @setEvalBranchQuota(10000);
+    const env = Env.loadFromPathComptime("env/multiline.env");
 
-test "load multiline" {
-    @setEvalBranchQuota(100000);
-    const env: Env = .loadFromPathComptime("env/multiline.env");
+    std.debug.print("\n", .{});
     env.print();
-}
 
+    try testing.expectEqualStrings("basic", env.get("BASIC").?);
+    try testing.expectEqualStrings("after_line", env.get("AFTER_LINE").?);
+    try testing.expectEqualStrings("", env.get("EMPTY").?);
+    try testing.expectEqualStrings("single_quotes", env.get("SINGLE_QUOTES").?);
+    try testing.expectEqualStrings("    single quotes    ", env.get("SINGLE_QUOTES_SPACED").?);
+    try testing.expectEqualStrings("double_quotes", env.get("DOUBLE_QUOTES").?);
+    try testing.expectEqualStrings("    double quotes    ", env.get("DOUBLE_QUOTES_SPACED").?);
+    try testing.expectEqualStrings("expand\nnew\nlines", env.get("EXPAND_NEWLINES").?);
+    try testing.expectEqualStrings("dontexpand\\nnewlines", env.get("DONT_EXPAND_UNQUOTED").?);
+    try testing.expectEqualStrings("dontexpand\\nnewlines", env.get("DONT_EXPAND_SQUOTED").?);
+    try testing.expectEqualStrings("equals==", env.get("EQUAL_SIGNS").?);
+    try testing.expectEqualStrings("{\"foo\": \"bar\"}", env.get("RETAIN_INNER_QUOTES").?);
+    try testing.expectEqualStrings("{\"foo\": \"bar\"}", env.get("RETAIN_INNER_QUOTES_AS_STRING").?);
+    try testing.expectEqualStrings("some spaced out string", env.get("TRIM_SPACE_FROM_UNQUOTED").?);
+    try testing.expectEqualStrings("therealnerdybeast@example.tld", env.get("USERNAME").?);
+    try testing.expectEqualStrings("parsed", env.get("SPACED_KEY").?);
+
+    try testing.expectEqualStrings(
+        \\THIS
+        \\IS
+        \\A
+        \\MULTILINE
+        \\STRING
+    , env.get("MULTI_DOUBLE_QUOTED").?);
+
+    try testing.expectEqualStrings(
+        \\THIS
+        \\IS
+        \\A
+        \\MULTILINE
+        \\STRING
+    , env.get("MULTI_SINGLE_QUOTED").?);
+
+    try testing.expectEqualStrings(
+        \\THIS
+        \\IS
+        \\A
+        \\"MULTILINE'S"
+        \\STRING
+    , env.get("MULTI_BACKTICKED").?);
+
+    try testing.expectEqualStrings(env.get("MULTI_PEM_DOUBLE_QUOTED").?[0..26], "-----BEGIN PUBLIC KEY-----");
+}
